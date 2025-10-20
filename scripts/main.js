3eaf3befeed642292b97bdd75b5ca701bbdc5d24
Benson Mose <mosebemos26@gmail.com>
2025-10-20 16:10:18 +0300

    worked on the hamburger menu that works on small screens

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,11 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
